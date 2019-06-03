baefc7a31c317cb5ac0b300f1fb76868232066cb
yuanyazhen <yuanyazhen@didiglobal.com>
2019-06-03 16:13:52 +0800

    feat: fix config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,16 @@
 module.exports = {
   base: '/the-road-to-learn-fe/',
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: '前端入门',
+  description: 'the road to learn FE for my friends, enjoy it /:heart!!!',
+  // 注入到当前页面的 HTML <head> 中的标签
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: 'Last Updated' // 文档更新时间：每个文件git最后提交的时间
+  }
 }
